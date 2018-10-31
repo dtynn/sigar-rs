cb553889f9a990282552ecf57af366357e549b07
dtynn <dtynn@163.com>
2018-10-31 10:05:08 +0800

    add sigar-sys

---
+++ sigar-sys/build.rs
@@ -0,0 +1,18 @@
+extern crate cmake;
+
+use std::path;
+
+fn main() {
+    let dst = cmake::Config::new("sigar")
+        .static_crt(true)
+        .very_verbose(true)
+        .build_target("install")
+        .build();
+
+    println!(
+        "cargo:rustc-link-search=native={}",
+        path::Path::new(&dst).join("lib").display(),
+    );
+
+    println!("cargo:rustc-link-lib=static=sigar");
+}
