e763437b5f7f7ff05857d23ebd3a66cfb03c90a2
dtynn <dtynn@163.com>
2018-11-01 10:51:49 +0800

    add example code

--- sigar-sys/examples/info.rs
+++ sigar-sys/examples/info.rs
@@ -1,39 +1,3 @@
-### sigar-sys
-low level binding to [AlexYaruki/sigar](https://github.com/AlexYaruki/sigar), which is a independent fork version of official [hyperic/sigar](https://github.com/hyperic/sigar).
-
-
-#### Usage
-Add this to your `Cargo.toml`
-```toml
-[dependencies]
-sigar-sys = "1"
-```
-
-and this to your crate root:
-
-```rust
-extern crate sigar_sys;
-```
-
-
-
-#### Notes
-
-- As a `-sys` crate, this provides only the binding to sigar.
-
-  The sigar documentation itself is at [SourceForge/sigar](https://sourceforge.net/projects/sigar/).
-
-- The build script for this crate always makes a static linking.
-
-- This crate has been tested to work on Windows(with msvc), Linux and macOS.
-
-  Other platforms or envs may work, and PRs to support them are welcome.
-
-
-
-#### Example
-
-```rust
 extern crate sigar_sys;
 
 use sigar_sys as ffi;
@@ -99,10 +63,3 @@ unsafe fn pidlist() -> Result<Vec<ffi::sigar_pid_t>, String> {
 
     Ok(infos)
 }
-
-```
-
-
-
-**to run the the example code on windows, we should link 3 more libs: `ws2_32` `netapi32`, `version`, maybe in the build scripts**
-
